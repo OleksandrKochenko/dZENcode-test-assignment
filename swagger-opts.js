00a169c81ce6ff925c3c18d852adb350bb094ea8
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-02-01 22:53:05 +0200

    swagger custom site title and favicon

--- swagger-opts.js
+++ swagger-opts.js
@@ -1,4 +1,7 @@
 const swaggerOptions = {
+  customSiteTitle: "Comments web API",
+  customfavIcon:
+    "https://res.cloudinary.com/dvloxectq/image/upload/v1706820529/comments/img/Custom-Icon-Design-Flatastic-1-Comment.512_d5v5k8.png",
   customJs: [
     "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
