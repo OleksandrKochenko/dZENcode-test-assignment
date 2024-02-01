6f700ac70c488158aa9b268ef3d8953ad1507416
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-02-01 21:36:31 +0200

    swagger custom js

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use("/", swaggerUi.serve);
 app.get(
   "/",
   swaggerUi.setup(swaggerDocument, {
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
     customCssUrl: [
       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
       "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
