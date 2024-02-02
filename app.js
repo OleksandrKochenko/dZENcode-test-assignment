7b17f35cc7d50e4cfa22f7735698a0a827d60e68
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-02-02 20:02:43 +0200

    set tmp folder in express (test)

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const apiRouter = require("./routes/api-routes");
 const app = express();
 
 app.set("views", path.join(__dirname, "/views"));
+app.set("tmp", path.join(__dirname, "/tmp"));
 app.set("view engine", "ejs");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
